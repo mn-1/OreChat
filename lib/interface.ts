4492f9b87910e5918d7d43d25ceacd8e5b2ac6b7
dev-neo2302 <105268593+mn-1@users.noreply.github.com>
2024-08-05 11:04:05 +0900

    initial

--- lib/interface.ts
+++ lib/interface.ts
@@ -1,16 +1,17 @@
-import { AuthToken } from "@skyway-sdk/core";
-
+// APIのレスポンスの型を定義
 export interface ApiResponse<T> {
-  isSuccess: boolean,
-  body: T
+  isSuccess: boolean;
+  body?: T;
 }
 
+// エラーの型を定義
 export interface CustomError {
-  errorCode?: Number,
-  errorMessage: string
+  errorCode?: Number;
+  message: string;
 }
 
-export interface MySkywayAuthInfo {
-  skywayToken: string,
-  jwt: AuthToken
-}
+type UndefinedOrOptional<T> = T extends undefined ? {} : { body: T };
+
+export type ActionResult<T> =
+  | ({ isSuccess: true } & UndefinedOrOptional<T>)
+  | { isSuccess: false; message: string };
